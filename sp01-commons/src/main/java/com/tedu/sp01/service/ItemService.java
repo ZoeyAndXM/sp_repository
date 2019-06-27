e1c5cec0beb9232ad04490b91d99fe811737893e
全日制 <@DESKTOP-J9KSVUM>
2019-06-27 17:22:38 +0800

    akhda

--- sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -5,7 +5,8 @@ import java.util.List;
 import com.tedu.sp01.pojo.Item;
 
 public interface ItemService {
+	//dajda
 	List<Item> getItems(String orderId);
 	void decreaseNumbers(List<Item> list);
-	//banbenceshi
+	
 }
