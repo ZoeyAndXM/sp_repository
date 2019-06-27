9e25c87b24c810b3a74d752d30e8f7f514196dc1
全日制 <@DESKTOP-J9KSVUM>
2019-06-27 19:31:15 +0800

    加了注释

--- sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -5,11 +5,10 @@ import java.util.List;
 import com.tedu.sp01.pojo.Item;
 
 public interface ItemService {
-	//dajda22dada
-	/*
-	 * 	我特fuck?56464646
-	 *  46546646.fd
-	 *  4646768广泛的关注
+	/**
+	 * 	加了注释
+	 * @param orderId
+	 * @return
 	 */
 	List<Item> getItems(String orderId);
 	void decreaseNumbers(List<Item> list);
